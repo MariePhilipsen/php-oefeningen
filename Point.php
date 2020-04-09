71b2aabec9e675b3453f14da2c6eb45bf1b688a0
MariePhilipsen <marie_philipsen@hotmail.com>
2020-04-09 12:18:28 +0200

    adding static variable count + static method getCount()

--- Point.php
+++ Point.php
@@ -5,10 +5,21 @@ final class Point
     private $x;
     private $y;
 
+    // static variabelen en methoden aanroepen met self::
+
+    private static $count = 0;
+
+    //magic method beginnen steeds met __
     public function __construct($x,$y)
     {
         $this ->x = $x;
         $this ->y = $y;
+        self::$count++;
+    }
+
+    public function __destruct()
+    {
+        self::$count--;
     }
 
     function __toString()
@@ -20,5 +31,9 @@ final class Point
     {
         return sqrt(($this->x -$point->x)*($this->x - $point->x)+
             ($this->y -$point->y)*($this->y - $point->y));
+    }
+
+    public static function getCount(){
+        return self::$count;
     }
 }
--- app.php
+++ app.php
@@ -9,4 +9,11 @@ $point2 = new Point(5,11);
 print($point2.PHP_EOL);
 
 $distance = $point1->calculateDistance($point2);
-print($distance);
+print($distance.PHP_EOL);
+
+// static method wordt opgeroepen op klasse (Point) gevolgd door ::
+print("aantal aangemaakte Point-objecten:".Point::getCount().PHP_EOL);
+
+unset($point1);
+
+print("aantal aangemaakte Point-objecten:".Point::getCount());
